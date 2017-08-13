f671afc6d090da6f41367c8f6f1a03dd28651b55
Scott M Stark <starksm64@gmail.com>
2017-08-13 13:46:02 -0700

    The Claim value needs to be binding since @RequestScoped producers cannot have the InjectionPoint injected
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- api/src/main/java/org/eclipse/microprofile/jwt/Claim.java
+++ api/src/main/java/org/eclipse/microprofile/jwt/Claim.java
@@ -19,7 +19,6 @@
  */
 package org.eclipse.microprofile.jwt;
 
-import javax.enterprise.util.Nonbinding;
 import javax.inject.Qualifier;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -38,5 +37,5 @@ public @interface Claim {
      * @return the claim name
      * @see JsonWebToken#getClaim(String)
      */
-    @Nonbinding String value();
+    String value();
 }
